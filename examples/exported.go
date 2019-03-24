94db559a5ce0446425174bd5b0c9cac885850809
Ryan Moran <rmoran@pivotal.io>
2019-03-24 14:57:07 -0400

    Removes examples

--- examples/exported.go
+++
@@ -1,6 +0,0 @@
-package examples
-
-//go:generate faux -i Exported -o fakes/exported.go
-type Exported interface {
-	Do(arg1 string, arg2 int) (output1 bool, output2 error)
-}
--- examples/other.go
+++
@@ -1,6 +0,0 @@
-package examples
-
-//go:generate faux -i Other -o fakes/other.go
-type Other interface {
-	Execute(arg1 string, arg2 int) (output1 bool, output2 error)
-}
