2b6860b43c09cdf5e78f2681c419d34a17b5a671
Ryan Moran <rmoran@vmware.com>
2022-05-05 09:00:51 -0700

    Only pre-import packages in the same file as interface

--- go.mod
+++ go.mod
@@ -1,10 +1,24 @@
 module github.com/ryanmoran/faux
 
+go 1.18
+
 require (
+	github.com/BurntSushi/toml v1.1.0
 	github.com/onsi/ginkgo v1.16.5
 	github.com/onsi/gomega v1.19.0
+	github.com/pelletier/go-toml/v2 v2.0.0
 	github.com/pivotal-cf/jhanda v0.0.0-20181025233525-e6aa09a032df
 	golang.org/x/tools v0.1.10
 )
 
-go 1.13
+require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/nxadm/tail v1.4.8 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- parsing/parse.go
+++ parsing/parse.go
@@ -1,8 +1,15 @@
 package parsing
 
 import (
+	"bytes"
 	"fmt"
+	"go/parser"
+	"go/printer"
+	"go/token"
 	"go/types"
+	"os"
+	"runtime"
+	"strconv"
 
 	"golang.org/x/tools/go/packages"
 )
@@ -30,12 +37,45 @@ func Parse(path, name string) (Fake, error) {
 		return Fake{}, fmt.Errorf("failed to load named type: %s.%s", path, name)
 	}
 
+	filename := os.Expand(pkgs[0].Fset.File(object.Pos()).Name(), func(name string) string {
+		if name == "GOROOT" {
+			return runtime.GOROOT()
+		}
+
+		return os.Getenv(name)
+	})
+
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, filename, nil, parser.ImportsOnly)
+	if err != nil {
+		return Fake{}, fmt.Errorf("failed to parse scope file: %w", err)
+	}
+
 	var imports []Import
-	for _, p := range pkg.Imports() {
-		imports = append(imports, Import{
-			Name: p.Name(),
-			Path: p.Path(),
-		})
+	for _, i := range file.Imports {
+		buffer := bytes.NewBuffer(nil)
+
+		var name string
+		if i.Name != nil {
+			err = printer.Fprint(buffer, fset, i.Name)
+			if err != nil {
+				return Fake{}, fmt.Errorf("failed to print import package name: %w", err)
+			}
+		}
+		name = buffer.String()
+
+		buffer.Reset()
+		err = printer.Fprint(buffer, fset, i.Path)
+		if err != nil {
+			return Fake{}, fmt.Errorf("failed to print import package path: %w", err)
+		}
+
+		path, err := strconv.Unquote(buffer.String())
+		if err != nil {
+			return Fake{}, fmt.Errorf("failed to unquote import package path: %w", err)
+		}
+
+		imports = append(imports, Import{Name: name, Path: path})
 	}
 
 	iface, err := NewInterface(namedType)
